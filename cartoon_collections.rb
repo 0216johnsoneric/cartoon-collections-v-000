23a3fc3b774f066e44e957940e0b8b8debcde96a
Eric Johnson <0216johnsoneric@gmail.com>
2017-12-09 18:01:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,22 +5,22 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
 end
 
-# def summon_captain_planet(array)# code an argument here
-#   array.collect { |ring|
-#     return ring.capitalize + "!"
-#   }# Your code here
-#
-# end
+def summon_captain_planet(array)# code an argument here
+  array.collect { |ring|
+    return ring.capitalize + "!"
+  }# Your code here
 
-def summon_captain_planet(array)
-  nu_array = []
-  i = 0
-  while i < array.length
-    nu_array << array[i].capitalize + "!"
-    i += 1
-  end
-  nu_array
 end
+
+# def summon_captain_planet(array)
+#   nu_array = []
+#   i = 0
+#   while i < array.length
+#     nu_array << array[i].capitalize + "!"
+#     i += 1
+#   end
+#   nu_array
+# end
 
 def long_planeteer_calls(array)
   i = 0
